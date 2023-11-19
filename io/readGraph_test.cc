b134a5d96f65272e36593342cdf95b14cfd9b16a
alvarogarmen <alvarogarmendia0@gmail.com>
2023-11-19 20:47:54 +0100

    Updated testcases. I have a problem with absl and cannot run them tho

--- io/readGraph_test.cc
+++ io/readGraph_test.cc
@@ -27,10 +27,84 @@ TEST(ReadGraphTest, ValidGraph) {
   std::remove(testFileName.c_str());
 }
 
+// Test case for invalid header format
+TEST(ReadGraphTest, InvalidHeaderFormat) {
+  std::stringstream testData;
+  // Invalid header format: missing 'p' or incorrect format
+  testData << "c this is a comment and should not be read" << std::endl;
+  testData << "invalid_header" << std::endl;
+
+  auto result = readGraph<BipartiteGraph>(testData);
+  ASSERT_FALSE(result.ok());
+  ASSERT_EQ(result.status().code(), absl::StatusCode::kInvalidArgument);
+}
+
+// Test case for invalid line format
+TEST(ReadGraphTest, InvalidLineFormat) {
+  std::stringstream testData;
+  // Invalid line format: characters instead of integers
+  testData << "c this is a comment and should not be read" << std::endl;
+  testData << "p ocr 3 4 5" << std::endl;
+  testData << "invalid line" << std::endl;
+
+  auto result = readGraph<BipartiteGraph>(testData);
+  ASSERT_FALSE(result.ok());
+  ASSERT_EQ(result.status().code(), absl::StatusCode::kInvalidArgument);
+}
+
+// Test case for nodes out of bounds
+TEST(ReadGraphTest, NodesOutOfBounds) {
+  std::stringstream testData;
+  // Nodes out of bounds: source node greater than n0
+  testData << "c this is a comment and should not be read" << std::endl;
+  testData << "p ocr 3 4 5" << std::endl;
+  testData << "4 2" << std::endl;  // Source node is out of bounds
+
+  auto result = readGraph<BipartiteGraph>(testData);
+  ASSERT_FALSE(result.ok());
+  ASSERT_EQ(result.status().code(), absl::StatusCode::kInvalidArgument);
+}
+
+// Test case for negative number of nodes or edges
+TEST(ReadGraphTest, NegativeNumbers) {
+  std::stringstream testData;
+  // Negative number of nodes
+  testData << "c this is a comment and should not be read" << std::endl;
+  testData << "p ocr -3 4 5" << std::endl;  // Negative n0
+
+  auto result = readGraph<BipartiteGraph>(testData);
+  ASSERT_FALSE(result.ok());
+  ASSERT_EQ(result.status().code(), absl::StatusCode::kInvalidArgument);
+}
+
+// Test case for missing 'p' line
+TEST(ReadGraphTest, MissingPLine) {
+  std::stringstream testData;
+  // Missing 'p' line
+  testData << "c this is a comment and should not be read" << std::endl;
+  testData << "1 2" << std::endl;  // Edge line without 'p' line
+
+  auto result = readGraph<BipartiteGraph>(testData);
+  ASSERT_FALSE(result.ok());
+  ASSERT_EQ(result.status().code(), absl::StatusCode::kInvalidArgument);
+}
+
+// Test case for file with invalid content
+TEST(ReadGraphTest, InvalidFileContent) {
+  std::stringstream testData;
+  // Invalid content: non-integer characters instead of number of nodes/edges
+  testData << "c this is a comment and should not be read" << std::endl;
+  testData << "p ocr abc 4 5" << std::endl;  // Invalid n0
+
+  auto result = readGraph<BipartiteGraph>(testData);
+  ASSERT_FALSE(result.ok());
+  ASSERT_EQ(result.status().code(), absl::StatusCode::kInvalidArgument);
+}
+
 // Test case to check if the function handles errors correctly.
 TEST(ReadGraphTest, ErrorHandling) {
-  // Test with a non-existing file. Please, never write a non_existent.txt file
-  const std::string nonExistentFileName = "non_existent.txt";
+  // Test with a non-existing file/empty stringstream
+  const std::stringstream nonExistentFileName;
   auto result = readGraph<BipartiteGraph>(nonExistentFileName);
   ASSERT_FALSE(result.ok());
   ASSERT_EQ(result.status().code(), absl::StatusCode::kNotFound);
