cd77c77356076e8e1a4ee91cc44517166cdcce65
Tomer Haham <nh271@stud.uni-heidelberg.de>
2023-12-09 09:58:02 +0100

    add compute_crossings_test

--- oscm/reduction_algorithm/reduction_algorithm_test.cc
+++ oscm/reduction_algorithm/reduction_algorithm_test.cc
@@ -6,38 +6,6 @@
 #include "gmock/gmock-matchers.h"
 #include "gtest/gtest.h"
 
-TEST(AlgorithmTest, computeCrossingSum) {
-  std::vector<std::vector<int>> freeNodes = {{0, 1}, {0}, {0, 1, 2}};
-  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {0, 2}, {2}};
-  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
-  UndoAlgorithmStep undo = UndoAlgorithmStep<int, int>();
-
-  computeCrossingSums<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
-  EXPECT_EQ(myGraph.getFreeNodesSize(), 3);
-  EXPECT_EQ(myGraph.getFixedNodesSize(), 3);
-  EXPECT_EQ(myGraph.getFreeNodeNeighboursSize(0), 2);
-  EXPECT_EQ(myGraph.getFreeNodeNeighbours(0)[1], 1);
-  EXPECT_EQ(myGraph.getNodeCrossing(0).at(2), 1);
-  EXPECT_EQ(myGraph.getNodeCrossing(2).at(0), 3);
-  EXPECT_EQ(myGraph.getNodeCrossing(0).size(), 1);
-  EXPECT_EQ(myGraph.getNodeCrossing(1).size(), 0);
-  EXPECT_EQ(myGraph.getNodeCrossing(2).size(), 1);
-
-  EXPECT_EQ(myGraph.getLeftNodes(0).size(), 1);
-  EXPECT_EQ(myGraph.getRightNodes(0).size(), 0);
-  EXPECT_EQ(myGraph.getLeftNodes(1).size(), 0);
-  EXPECT_EQ(myGraph.getRightNodes(1).size(), 2);
-  EXPECT_EQ(myGraph.getLeftNodes(2).size(), 1);
-  EXPECT_EQ(myGraph.getRightNodes(2).size(), 0);
-  EXPECT_EQ(*(myGraph.getLeftNodes(0).find(1)), 1);
-  EXPECT_EQ(*(myGraph.getRightNodes(1).find(0)), 0);
-  EXPECT_EQ(*(myGraph.getRightNodes(1).find(2)), 2);
-
-  EXPECT_EQ(myGraph.getNodeCrossing(0).size(), 1);
-  EXPECT_EQ(myGraph.getNodeCrossing(1).size(), 0);
-  EXPECT_EQ(myGraph.getNodeCrossing(2).size(), 1);
-}
-
 TEST(AlgorithmTest, ParameterAccounting) {
   std::vector<std::vector<int>> freeNodes = {{0, 2}, {0, 3}, {0, 1}, {1, 3}};
   std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {2, 3}, {0}, {1, 3}};
