fa145c1ee2b95a044bc881ef541eb78cdfe3d25f
TomerHaham <131377578+TomerHaham@users.noreply.github.com>
2024-01-13 13:23:29 +0100

    Reduction algorithm (#21)
    * reduction graph data structure + test
    
    * add reductions + some tests
    
    * add rr3 test
    
    * add undo
    
    * add rr2 test
    
    * add rrLarge
    
    * add algorithm step
    
    * add algorithm
    
    * fix some review comments
    
    * fix some review comments
    
    * fix some review comments
    
    * fix some review comments
    
    * fix some review comments
    
    * add comment
    
    * remove launch.json
    
    * Fix review comments
    
    * add compute_crossings_test
    
    * rename UndoAlgorithmStep
    
    * fix review comments
    
    * rename undoAlgorithmStep
    
    * add comment
    
    * Fix review comments
    
    * add algorithm test + fix bug
    
    * fix recursion
    
    ---------
    
    Co-authored-by: Tomer Haham <nh271@stud.uni-heidelberg.de>

---
+++ oscm/reduction_algorithm/compute_crossings_test.cc
@@ -0,0 +1,47 @@
+#include <map>
+#include <vector>
+
+#include "gmock/gmock-matchers.h"
+#include "gtest/gtest.h"
+#include "reduction_algorithm.h"
+
+TEST(ComputeCrossings, computeUVcrossing) {
+  std::vector<std::vector<int>> freeNodes = {{0, 1}, {0}, {0, 1, 2}};
+  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {0, 2}, {2}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  UndoAlgorithmStep undo = UndoAlgorithmStep<int, int>();
+  EXPECT_EQ((computeUVcrossing<ReductionGraph<int, int>>(myGraph, 0, 2)), 1);
+  EXPECT_EQ((computeUVcrossing<ReductionGraph<int, int>>(myGraph, 1, 0)), 0);
+  EXPECT_EQ((computeUVcrossing<ReductionGraph<int, int>>(myGraph, 2, 0)), 3);
+}
+
+TEST(ComputeCrossings, computeCrossingSum) {
+  std::vector<std::vector<int>> freeNodes = {{0, 1}, {0}, {0, 1, 2}};
+  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {0, 2}, {2}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+
+  computeCrossingSums<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+  EXPECT_EQ(myGraph.getFreeNodesSize(), 3);
+  EXPECT_EQ(myGraph.getFixedNodesSize(), 3);
+  EXPECT_EQ(myGraph.getFreeNodeNeighboursSize(0), 2);
+  EXPECT_EQ(myGraph.getFreeNodeNeighbours(0)[1], 1);
+  EXPECT_EQ(myGraph.getNodeCrossing(0).at(2), 1);
+  EXPECT_EQ(myGraph.getNodeCrossing(2).at(0), 3);
+  EXPECT_EQ(myGraph.getNodeCrossing(0).size(), 1);
+  EXPECT_EQ(myGraph.getNodeCrossing(1).size(), 0);
+  EXPECT_EQ(myGraph.getNodeCrossing(2).size(), 1);
+
+  EXPECT_EQ(myGraph.getLeftNodes(0).size(), 1);
+  EXPECT_EQ(myGraph.getRightNodes(0).size(), 0);
+  EXPECT_EQ(myGraph.getLeftNodes(1).size(), 0);
+  EXPECT_EQ(myGraph.getRightNodes(1).size(), 2);
+  EXPECT_EQ(myGraph.getLeftNodes(2).size(), 1);
+  EXPECT_EQ(myGraph.getRightNodes(2).size(), 0);
+  EXPECT_EQ(*(myGraph.getLeftNodes(0).find(1)), 1);
+  EXPECT_EQ(*(myGraph.getRightNodes(1).find(0)), 0);
+  EXPECT_EQ(*(myGraph.getRightNodes(1).find(2)), 2);
+
+  EXPECT_EQ(myGraph.getNodeCrossing(0).size(), 1);
+  EXPECT_EQ(myGraph.getNodeCrossing(1).size(), 0);
+  EXPECT_EQ(myGraph.getNodeCrossing(2).size(), 1);
+}
