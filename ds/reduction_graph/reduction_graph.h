96923fcf4a46627273279c452e5d68d05d4cc9da
alvarogarmen <alvarogarmendia0@gmail.com>
2024-01-14 19:05:43 +0100

    Rebasing

---
+++ ds/reduction_graph/reduction_graph.h
@@ -0,0 +1,117 @@
+#pragma once
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <vector>
+
+#include "ds/reduction_graph/undo_algorithm_step.h"
+
+template <typename NT, typename CCT>
+class ReductionGraph {
+  // for each free node holds its neighbours
+  std::vector<std::vector<NT>> freeNodes;
+  // for each fixed node holds its neighbours
+  std::vector<std::vector<NT>> fixedNodes;
+  // holds the end position of free nodes
+  std::vector<NT> fixedPosition;
+  // for each free node u, the first place hold free node that are knowned to be positioned to the
+  // left of u
+  // and the second hold the free nodes that are knowned to be positioned to its right
+  std::vector<std::array<std::set<NT>, 2>> leftRightSet;
+  // save the crossing number that accur between two free nodes (u, v) ,that do not have < order
+  // yet.
+  // saves the crossing number of u and v assuming u is placed before v
+  std::vector<std::map<NT, CCT>> crossings;
+
+ public:
+  using NodeType = NT;
+  using CrossingCountType = CCT;
+  ReductionGraph(const std::vector<std::vector<NodeType>>& freeNodes,
+                 const std::vector<std::vector<NodeType>>& fixedNodes)
+      : freeNodes(freeNodes),
+        fixedNodes(fixedNodes),
+        fixedPosition(freeNodes.size()),
+        leftRightSet(freeNodes.size()),
+        crossings(freeNodes.size()) {}
+
+  ReductionGraph(NodeType numFreeNodes, NodeType numFixedNodes)
+      : freeNodes(numFreeNodes, std::vector<NodeType>(0)),
+        fixedNodes(numFreeNodes, std::vector<NodeType>(0)),
+        fixedPosition(freeNodes.size()),
+        leftRightSet(freeNodes.size()),
+        crossings(freeNodes.size()) {}
+
+  NodeType getFixedNodesSize() const { return fixedNodes.size(); }
+
+  NodeType getFixedNodeNeighboursSize(NodeType nodeID) const { return fixedNodes[nodeID].size(); }
+
+  const auto& getFixedNodeNeighbours(NodeType fixedNodeID) const { return fixedNodes[fixedNodeID]; }
+
+  NodeType getFreeNodesSize() const { return freeNodes.size(); }
+
+  NodeType getFreeNodeNeighboursSize(NodeType nodeID) const { return freeNodes[nodeID].size(); }
+
+  const auto& getFreeNodeNeighbours(NodeType freeNodeID) const { return freeNodes[freeNodeID]; }
+
+  const auto& getNodeCrossing(NodeType u) const { return crossings[u]; }
+
+  const auto& getCrossing(NodeType u, NodeType v) const { return crossings[u].at(v); }
+
+  const auto& getLeftNodes(NodeType u) const { return leftRightSet[u][0]; }
+
+  void insertRightNode(NodeType u, NodeType v) { leftRightSet[u][1].insert(v); }
+
+  void insertLeftNode(NodeType u, NodeType v) { leftRightSet[u][0].insert(v); }
+
+  const auto& getRightNodes(NodeType u) const { return leftRightSet[u][1]; }
+
+  void setLeftNodes(NodeType u, std::set<NodeType> leftNodes) { leftRightSet[u][0] = leftNodes; }
+
+  void setRightNodes(NodeType u, std::set<NodeType> rightNodes) { leftRightSet[u][1] = rightNodes; }
+
+  const auto& getFixedPosition() const { return fixedPosition; }
+
+  void setFixedPosition(NodeType u, NodeType index) { fixedPosition[index] = u; }
+
+  void setCrossings(const std::vector<std::map<NodeType, CrossingCountType>>& m) { crossings = m; }
+
+  void setFreeNodes(const std::vector<std::vector<NodeType>>& newfreeNodes) {
+    freeNodes = newfreeNodes;
+  }
+
+  void deleteLeftNode(NodeType node, NodeType leftNode) { leftRightSet[node][0].erase(leftNode); }
+
+  void deleteRightNode(NodeType node, NodeType rightNode) {
+    leftRightSet[node][1].erase(rightNode);
+  }
+
+  void addCrossing(NodeType leftNode, NodeType rightNode, CrossingCountType crossingSum) {
+    crossings[leftNode][rightNode] = crossingSum;
+  }
+
+  void deleteCrossings(NodeType u, NodeType v) {
+    crossings[u].erase(v);
+    crossings[v].erase(u);
+  }
+
+  void doUndo(UndoAlgorithmStep<NodeType, CrossingCountType>& undo) {
+    for (const auto& operation : undo.getParameterAccountingUndo()) {
+      deleteLeftNode(operation.rightNode, operation.leftNode);
+      deleteRightNode(operation.leftNode, operation.rightNode);
+      addCrossing(operation.leftNode, operation.rightNode, operation.leftRightCrossing);
+      addCrossing(operation.rightNode, operation.leftNode, operation.rightLeftCrossing);
+    }
+    for (const auto& position : undo.getSetPositionUndo()) {
+      setFixedPosition(0, position);
+    }
+  }
+
+  void clearLeftRightSet() {
+    for (NodeType u = 0; u < freeNodes.size(); ++u) {
+      leftRightSet[u][0].clear();
+      leftRightSet[u][1].clear();
+    }
+  }
+};
---
+++ oscm/reduction_algorithm/reduction_algorithm_test.cc
@@ -0,0 +1,228 @@
+#include "reduction_algorithm.h"
+
+#include <map>
+#include <vector>
+
+#include "gmock/gmock-matchers.h"
+#include "gtest/gtest.h"
+
+TEST(AlgorithmTest, ParameterAccounting) {
+  std::vector<std::vector<int>> freeNodes = {{0, 2}, {0, 3}, {0, 1}, {1, 3}};
+  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {2, 3}, {0}, {1, 3}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  UndoAlgorithmStep undo = UndoAlgorithmStep<int, int>();
+  int currentSolution = 0;
+
+  computeCrossingSums<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+  parameterAccounting<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph, 0, 1,
+                                                                             currentSolution);
+  EXPECT_EQ(myGraph.getLeftNodes(0).size(), 0);
+  EXPECT_EQ(myGraph.getRightNodes(0).size(), 1);
+
+  EXPECT_EQ(myGraph.getLeftNodes(1).size(), 1);
+  EXPECT_EQ(myGraph.getRightNodes(1).size(), 0);
+
+  EXPECT_EQ(myGraph.getLeftNodes(2).size(), 0);
+  EXPECT_EQ(myGraph.getRightNodes(2).size(), 1);
+
+  EXPECT_EQ(myGraph.getLeftNodes(3).size(), 1);
+  EXPECT_EQ(myGraph.getRightNodes(3).size(), 0);
+  EXPECT_EQ(*(myGraph.getRightNodes(0).find(1)), 1);
+  EXPECT_EQ(*(myGraph.getLeftNodes(1).find(0)), 0);
+  EXPECT_EQ(currentSolution, 1);
+  // now check transitvity
+  parameterAccounting<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph, 1, 2,
+                                                                             currentSolution);
+  EXPECT_EQ(myGraph.getLeftNodes(0).size(), 0);
+  EXPECT_EQ(myGraph.getRightNodes(0).size(), 3);
+
+  EXPECT_EQ(myGraph.getLeftNodes(1).size(), 1);
+  EXPECT_EQ(myGraph.getRightNodes(1).size(), 2);
+
+  EXPECT_EQ(myGraph.getLeftNodes(2).size(), 2);
+  EXPECT_EQ(myGraph.getRightNodes(2).size(), 1);
+
+  EXPECT_EQ(myGraph.getLeftNodes(3).size(), 3);
+  EXPECT_EQ(myGraph.getRightNodes(3).size(), 0);
+  EXPECT_EQ(*(myGraph.getRightNodes(0).find(1)), 1);
+  EXPECT_EQ(*(myGraph.getRightNodes(0).find(2)), 2);
+  EXPECT_EQ(*(myGraph.getRightNodes(0).find(3)), 3);
+  EXPECT_EQ(*(myGraph.getRightNodes(1).find(2)), 2);
+  EXPECT_EQ(*(myGraph.getRightNodes(1).find(3)), 3);
+  EXPECT_EQ(*(myGraph.getLeftNodes(1).find(0)), 0);
+  EXPECT_EQ(*(myGraph.getRightNodes(2).find(3)), 3);
+  EXPECT_EQ(*(myGraph.getLeftNodes(2).find(0)), 0);
+  EXPECT_EQ(*(myGraph.getLeftNodes(2).find(1)), 1);
+  EXPECT_EQ(*(myGraph.getLeftNodes(3).find(0)), 0);
+  EXPECT_EQ(*(myGraph.getLeftNodes(3).find(1)), 1);
+  EXPECT_EQ(*(myGraph.getLeftNodes(3).find(2)), 2);
+  EXPECT_EQ(currentSolution, 7);
+}
+
+TEST(AlgorithmTest, rrlo1Rrlo2) {
+  std::vector<std::vector<int>> freeNodes = {{0, 1}, {0}, {0, 1, 2}};
+  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {0, 2}, {2}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  int currentSolution = 0;
+  UndoAlgorithmStep undo = UndoAlgorithmStep<int, int>();
+
+  computeCrossingSums<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+  rrlo1<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph, &undo);
+  EXPECT_EQ(myGraph.getFixedPosition()[0], 1);
+  EXPECT_EQ(myGraph.getFixedPosition()[1], 0);
+  EXPECT_EQ(myGraph.getFixedPosition()[2], 0);
+  rrlo2<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph, currentSolution, &undo);
+  rrlo1<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph, &undo);
+  EXPECT_EQ(myGraph.getFixedPosition()[0], 1);
+  EXPECT_EQ(myGraph.getFixedPosition()[1], 0);
+  EXPECT_EQ(myGraph.getFixedPosition()[2], 2);
+}
+
+TEST(AlgorithmTest, rr3) {
+  std::vector<std::vector<int>> freeNodes = {{0, 2}, {0, 1}, {0, 2}};
+  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {1}, {0, 2}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  UndoAlgorithmStep undo = UndoAlgorithmStep<int, int>();
+  int currentSolution = 0;
+  computeCrossingSums<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+  rr3<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph, currentSolution, &undo);
+  EXPECT_EQ(myGraph.getLeftNodes(0).size(), 1);
+  EXPECT_EQ(myGraph.getRightNodes(0).size(), 0);
+  EXPECT_EQ(*(myGraph.getLeftNodes(0).find(1)), 1);
+
+  EXPECT_EQ(myGraph.getLeftNodes(1).size(), 0);
+  EXPECT_EQ(myGraph.getRightNodes(1).size(), 2);
+  EXPECT_EQ(*(myGraph.getRightNodes(1).find(0)), 0);
+  EXPECT_EQ(*(myGraph.getRightNodes(1).find(2)), 2);
+
+  EXPECT_EQ(myGraph.getLeftNodes(2).size(), 1);
+  EXPECT_EQ(myGraph.getRightNodes(2).size(), 0);
+  EXPECT_EQ(*(myGraph.getLeftNodes(2).find(1)), 1);
+  rrlo1<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph, &undo);
+  EXPECT_EQ(myGraph.getFixedPosition()[0], 1);
+}
+
+TEST(AlgorithmTest, rr2) {
+  std::vector<std::vector<int>> freeNodes = {{0, 2}, {0, 1}, {0, 2}};
+  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {1}, {0, 2}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  int currentSolution = 0;
+  computeCrossingSums<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+  rr2<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph, currentSolution);
+  EXPECT_EQ(myGraph.getLeftNodes(0).size(), 0);
+  EXPECT_EQ(myGraph.getRightNodes(0).size(), 1);
+
+  EXPECT_EQ(*(myGraph.getRightNodes(0).find(2)), 2);
+
+  EXPECT_EQ(myGraph.getLeftNodes(2).size(), 1);
+  EXPECT_EQ(myGraph.getRightNodes(2).size(), 0);
+
+  EXPECT_EQ(*(myGraph.getLeftNodes(2).find(0)), 0);
+}
+
+TEST(AlgorithmTest, rrLarge) {
+  std::vector<std::vector<int>> freeNodes = {{0, 1}, {0}, {0, 1, 2}};
+  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {0, 2}, {2}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  int currentSolution = 0;
+  computeCrossingSums<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+
+  rrLarge<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph, 2, currentSolution);
+  EXPECT_EQ(myGraph.getLeftNodes(0).size(), 1);
+  EXPECT_EQ(myGraph.getRightNodes(0).size(), 1);
+  EXPECT_EQ(myGraph.getRightNodes(2).size(), 0);
+  EXPECT_EQ(myGraph.getLeftNodes(2).size(), 2);
+}
+
+TEST(AlgorithmTest, IJBiggerThenFour) {
+  std::vector<std::vector<int>> freeNodes = {{0, 1}, {0}, {0, 1, 2}};
+  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {0, 2}, {2}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  computeCrossingSums<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+
+  auto [success, u, v] = IJBiggerThenFour<ReductionGraph<int, int>>(myGraph);
+  EXPECT_EQ(u, 0);
+  EXPECT_EQ(v, 2);
+}
+
+TEST(AlgorithmTest, IJEqualToThree) {
+  std::vector<std::vector<int>> freeNodes = {{0, 1}, {0}, {0, 2}};
+  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {0}, {2}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  computeCrossingSums<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+
+  auto [success, u, v] = IJEqualToThree<ReductionGraph<int, int>>(myGraph);
+  EXPECT_EQ(u, 2);
+  EXPECT_EQ(v, 0);
+}
+
+TEST(AlgorithmTest, IJEqualToTwo) {
+  std::vector<std::vector<int>> freeNodes = {{1, 2}, {0}, {0, 1, 2}};
+  std::vector<std::vector<int>> fixedNodes = {{1, 2}, {0, 2}, {0, 2}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  computeCrossingSums<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+
+  auto [success, u, v] = IJEqualToTwo<ReductionGraph<int, int>>(myGraph);
+  EXPECT_EQ(u, 0);
+  EXPECT_EQ(v, 2);
+}
+
+TEST(AlgorithmTest, algorithm) {
+  std::vector<std::vector<int>> freeNodes = {{0, 1}, {0}, {0, 1, 2}};
+  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {0, 2}, {2}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  auto [crossingSum, orderVector] =
+      algorithm<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+  EXPECT_EQ(crossingSum, 1);
+  EXPECT_EQ(orderVector[0], 1);
+  EXPECT_EQ(orderVector[1], 0);
+  EXPECT_EQ(orderVector[2], 2);
+}
+
+TEST(AlgorithmTest, algorithmV1) {
+  std::vector<std::vector<int>> freeNodes = {{5},    {6}, {7}, {8}, {0, 9},
+                                             {0, 9}, {1}, {2}, {3}, {4}};
+  std::vector<std::vector<int>> fixedNodes = {{4, 5}, {6}, {7}, {8}, {9},
+                                              {0},    {1}, {2}, {3}, {4, 5}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  auto [crossingSum, orderVector] =
+      algorithm<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+  EXPECT_EQ(crossingSum, 17);
+}
+
+TEST(GraphTest, UndoAlgorithmStep) {
+  std::vector<std::vector<int>> freeNodes = {{0, 1}, {0}, {0, 1, 2}};
+  std::vector<std::vector<int>> fixedNodes = {{0, 1, 2}, {0, 2}, {2}};
+  ReductionGraph myGraph = ReductionGraph<int, int>(freeNodes, fixedNodes);
+  UndoAlgorithmStep undo = UndoAlgorithmStep<int, int>();
+  computeCrossingSums<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(myGraph);
+
+  int currentSolution = 0;
+
+  parameterAccounting<ReductionGraph<int, int>, UndoAlgorithmStep<int, int>>(
+      myGraph, 0, 2, currentSolution, &undo);
+  EXPECT_EQ(undo.getParameterAccountingUndo().size(), 1);
+  EXPECT_EQ(undo.getParameterAccountingUndo()[0].leftNode, 0);
+  EXPECT_EQ(undo.getParameterAccountingUndo()[0].rightNode, 2);
+  EXPECT_EQ(undo.getParameterAccountingUndo()[0].leftRightCrossing, 1);
+  EXPECT_EQ(undo.getParameterAccountingUndo()[0].rightLeftCrossing, 3);
+  EXPECT_EQ(myGraph.getNodeCrossing(0).size(), 0);
+  EXPECT_EQ(myGraph.getNodeCrossing(1).size(), 0);
+  EXPECT_EQ(myGraph.getNodeCrossing(2).size(), 0);
+  EXPECT_EQ(myGraph.getLeftNodes(0).size(), 1);
+  EXPECT_EQ(myGraph.getLeftNodes(1).size(), 0);
+  EXPECT_EQ(myGraph.getLeftNodes(2).size(), 2);
+  myGraph.doUndo(undo);
+  EXPECT_EQ(myGraph.getNodeCrossing(0).size(), 1);
+  EXPECT_EQ(myGraph.getNodeCrossing(1).size(), 0);
+  EXPECT_EQ(myGraph.getNodeCrossing(2).size(), 1);
+  EXPECT_EQ(myGraph.getNodeCrossing(0).at(2), 1);
+  EXPECT_EQ(myGraph.getNodeCrossing(2).at(0), 3);
+
+  EXPECT_EQ(myGraph.getLeftNodes(0).size(), 1);
+  EXPECT_EQ(myGraph.getLeftNodes(1).size(), 0);
+  EXPECT_EQ(myGraph.getLeftNodes(2).size(), 1);
+  EXPECT_EQ(myGraph.getRightNodes(0).size(), 0);
+  EXPECT_EQ(myGraph.getRightNodes(1).size(), 2);
+  EXPECT_EQ(myGraph.getRightNodes(2).size(), 0);
+}
