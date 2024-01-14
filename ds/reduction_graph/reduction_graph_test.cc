72aa5da6b57843e486feed2cf00de0f7c6613c42
Tomer Haham <nh271@stud.uni-heidelberg.de>
2024-01-14 14:32:46 +0100

    constructor for  io + addEdge function

--- ds/reduction_graph/reduction_graph_test.cc
+++ ds/reduction_graph/reduction_graph_test.cc
@@ -26,3 +26,28 @@ TEST(GraphTest, SimpleTest) {
   EXPECT_EQ(myGraph.getNodeCrossing(1).size(), 0);
   EXPECT_EQ(myGraph.getNodeCrossing(2).size(), 0);
 }
+
+TEST(GraphTest, addEdge) {
+  ReductionGraph myGraph = ReductionGraph<int, int>(3, 3, 0);
+  myGraph.addEdge(0, 0);
+  myGraph.addEdge(0, 1);
+  myGraph.addEdge(1, 0);
+  myGraph.addEdge(2, 0);
+  myGraph.addEdge(2, 1);
+  myGraph.addEdge(2, 2);
+  EXPECT_EQ(myGraph.getFreeNodesSize(), 3);
+  EXPECT_EQ(myGraph.getFixedNodesSize(), 3);
+  EXPECT_EQ(myGraph.getFreeNodeNeighboursSize(0), 2);
+  EXPECT_EQ(myGraph.getFreeNodeNeighbours(0)[1], 1);
+
+  EXPECT_EQ(myGraph.getLeftNodes(0).size(), 0);
+  EXPECT_EQ(myGraph.getRightNodes(0).size(), 0);
+  EXPECT_EQ(myGraph.getLeftNodes(1).size(), 0);
+  EXPECT_EQ(myGraph.getRightNodes(1).size(), 0);
+  EXPECT_EQ(myGraph.getLeftNodes(2).size(), 0);
+  EXPECT_EQ(myGraph.getRightNodes(2).size(), 0);
+
+  EXPECT_EQ(myGraph.getNodeCrossing(0).size(), 0);
+  EXPECT_EQ(myGraph.getNodeCrossing(1).size(), 0);
+  EXPECT_EQ(myGraph.getNodeCrossing(2).size(), 0);
+}
