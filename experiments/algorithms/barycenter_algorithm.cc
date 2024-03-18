f24bb68f2fab08a41b7d6a453fc5167c71833b62
alvarogarmen <alvarogarmendia0@gmail.com>
2024-03-18 14:26:02 +0100

    Preparing experiments for barycenter (on bipartite_graph)

---
+++ experiments/algorithms/barycenter_algorithm.cc
@@ -0,0 +1,5 @@
+#include "experiments/algorithms/barycenter_algorithm.h"
+
+namespace oscm::experiments::algorithms {
+REGISTER_IMPL(BarycenterAlgorithm);
+}
---
+++ experiments/algorithms/barycenter_algorithm.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "ds/bipartite_graph.h"
+#include "ds/helper/empty_problem.h"
+#include "io/ocsm_graph_reader.h"
+#include "oscm/barycenter_algorithm/barycenter_algorithm.h"
+#include "toolkit/algorithms/algorithm_impl.h"
+#include "toolkit/app/app_io.pb.h"
+#include "toolkit/ds/empty_problem.h"
+
+namespace oscm::experiments::algorithms {
+struct BarycenterGraphEmptyProblem : public oscm::ds::EmptyProblem<BipartiteGraph<int>> {
+  const static constexpr std::string_view ds_name = "barycenter_graph";
+};
+namespace {
+using henrixapp::app::app_io::AlgorithmConfig;
+using henrixapp::app::app_io::AlgorithmRunInformation;
+using henrixapp::app::app_io::Hypergraph;
+using henrixapp::app::app_io::Result;
+using henrixapp::app::app_io::RunConfig;
+using BG = BarycenterGraphEmptyProblem;
+}  // namespace
+class BarycenterAlgorithm : public henrixapp::algorithms::AlgorithmImpl<BG> {
+ public:
+  static constexpr absl::string_view AlgorithmName = "barycenter_algorithm";
+
+ protected:
+  absl::StatusOr<std::unique_ptr<BG>> Execute(const AlgorithmConfig& config,
+                                              std::unique_ptr<BG> problem) override {
+    barycenter_algorithm::barycenterAlgorithm<BipartiteGraph<int>>(problem->instance());
+
+    return problem;
+  }
+  absl::Status ValidateConfig(const AlgorithmConfig& config) override { return absl::OkStatus(); }
+  absl::StatusOr<std::unique_ptr<BG>> Load(const RunConfig& run_config,
+                                           const Hypergraph& hypergraph) override {
+    // auto hgr = henrixapp::algorithms::loadFile<StandardIntegerHypergraph>(hypergraph);
+    if (hypergraph.format() == "gr") {
+      std::ifstream file(hypergraph.file_path());
+      auto Graph = readGraph<BipartiteGraph<int>>(file);
+      if (!Graph.ok()) {
+        return Graph.status();
+      }
+      return std::make_unique<BG>(std::move(Graph.value()));
+    }
+    return absl::UnimplementedError("Read-in-function for this format not implemented");
+  }
+};
+}  // namespace oscm::experiments::algorithms
